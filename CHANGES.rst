9db70cd11ab7aa40627413be41a977d60cc50b2e
Johannes Raggam <raggam-nl@adm.at>
2015-10-22 23:36:07 +0200

    Preparing release 1.1

--- CHANGES.rst
+++ CHANGES.rst
@@ -1,7 +1,7 @@
 Changelog
 =========
 
-1.1 (unreleased)
+1.1 (2015-10-22)
 ----------------
 
 - set json repsonse headers
--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ if sys.version_info < (2, 3):
 else:
     from setuptools import setup, find_packages
 
-version = '1.1.dev0'
+version = '1.1'
 
 setup(
     name='collective.jsonify',
